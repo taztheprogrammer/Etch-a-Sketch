ee1632622a04174338d0978a53675a0fd6f59ecd
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-08-01 10:13:32 -0400

    Implemented input, createGrid();
    Grid Size changes with user input

--- script.js
+++ script.js
@@ -2,20 +2,24 @@
 
 //gets the node for canvas div
 const canvas = document.getElementById('canvas');
+const displayBtn = document.getElementById('displayBtn');
+const valDisplay = document.getElementById('valDisplay');
+
+//const gridDim = 
 
 //creates 4 divs with the class of pixel in the canvas div
 for (let x=0; x<4; x++) {
     let temp = document.createElement('div');
     temp.classList.add('pixel');
     canvas.appendChild(temp);
+    temp.addEventListener('pointerenter', color);
 }
 
 //create constant 'pixels' a node list containing all pixel elements
-const pixels = document.querySelectorAll('.pixel');
 
-for (let x=0; x<pixels.length; x++) {
-    pixels[x].addEventListener('pointerenter', color);
-}
+let pixels = document.querySelectorAll('.pixel');
+
+
 
 function color() {
     event.target.style.backgroundColor = 'black';
@@ -23,9 +27,6 @@ function color() {
 
 /*
 what follows is selecting clear button
-implementing clearCanvas function for button
-clearCanvas function which iterates though the pixels nodelist
-and reduces background color
 */
 const clear = document.getElementById('clear');
 clear.addEventListener('click', clearCanvas);
@@ -34,19 +35,23 @@ function clearCanvas() {
     pixels.forEach(pixel => {
         pixel.style.backgroundColor = '';
     });
+} 
+
+
+displayBtn.addEventListener('click', createGrid);
+
+function createGrid() {
+    let gridVal = document.getElementById('gridVal');
+    let gridsize = gridVal.value;
+    valDisplay.innerHTML = gridsize + " * " + gridsize;
+    canvas.style.setProperty('--gridx', gridsize);
+    canvas.innerHTML = '';
+
+    for (let x=0; x<(gridsize**2); x++) {
+        let temp = document.createElement('div');
+        temp.classList.add('pixel');
+        canvas.appendChild(temp);
+        temp.addEventListener('pointerenter', color);
+    }
+    pixels = document.querySelectorAll('.pixel');
 }
-
-
-/* copy this
-<body>
-    <main>
-        <div id="control">
-            <button id="clear">clear</button>
-        </div>
-        <div id="canvas">
-        </div>
-    </main>
-    <script src="script.js"></script>
-</body>
-
-*/
