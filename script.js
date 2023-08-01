b6767dcc5f7a2724da5bc988fd89d48a60642007
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-08-01 09:20:53 -0400

    Pixels in grid are all functional.
    color function works and pixels injected with JS

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
 
 
-/*
-const pixels = document.getElementsByClassName("pixel");
-*/
+//gets the node for canvas div
 const canvas = document.getElementById('canvas');
 
-const one = document.createElement('div');
-one.classList.add('pixel');
-canvas.appendChild(one);
-
-one.addEventListener('pointerenter', color);
-
-function color() {
-    one.style.backgroundColor = 'black';
+//creates 4 divs with the class of pixel in the canvas div
+for (let x=0; x<4; x++) {
+    let temp = document.createElement('div');
+    temp.classList.add('pixel');
+    canvas.appendChild(temp);
 }
 
+//create constant 'pixels' a node list containing all pixel elements
+const pixels = document.querySelectorAll('.pixel');
 
+for (let x=0; x<pixels.length; x++) {
+    pixels[x].addEventListener('pointerenter', color);
+}
+
+function color() {
+    event.target.style.backgroundColor = 'black';
+}
+
+/*
+what follows is selecting clear button
+implementing clearCanvas function for button
+clearCanvas function which iterates though the pixels nodelist
+and reduces background color
+*/
 const clear = document.getElementById('clear');
 clear.addEventListener('click', clearCanvas);
 
 function clearCanvas() {
-    /*
-   for (let x = 0; x < pixels.length; x++) {
-    pixels[x].style.backgroundColor = '';
-   }
-   */
-  one.style.backgroundColor = "";
+    pixels.forEach(pixel => {
+        pixel.style.backgroundColor = '';
+    });
 }
+
+
+/* copy this
+<body>
+    <main>
+        <div id="control">
+            <button id="clear">clear</button>
+        </div>
+        <div id="canvas">
+        </div>
+    </main>
+    <script src="script.js"></script>
+</body>
+
+*/
